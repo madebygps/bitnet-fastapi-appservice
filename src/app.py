cd6125fbc6e9f5c1374246648b22ec693a91dcaa
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2025-05-02 21:35:24 -0400

    refactor to streaming response

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 import openai
 from dotenv import load_dotenv
 from pydantic import BaseModel
+import os
 
 app = FastAPI()
 load_dotenv()
@@ -12,10 +14,10 @@ load_dotenv()
 class ModelResponse(BaseModel):
     message: str
 
+BASE_DIR = Path(__file__).parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -26,9 +28,9 @@ async def index(request: Request):
 def chat(modelResponse: ModelResponse):
     user_message = modelResponse.message
     print(user_message)
-    client = openai.OpenAI(base_url="http://localhost:11434/v1", api_key="nokeyneeded")
+    client = openai.OpenAI(base_url=os.getenv('ENDPOINT'), api_key="nokeyneeded")
     response = client.chat.completions.create(
-        model="qwen3:0.6b",
+        model=os.getenv('MODEL'),
         temperature=0.7,
         n=1,
         messages=[
@@ -40,13 +42,16 @@ def chat(modelResponse: ModelResponse):
                 "role": "user",
                 "content": user_message,
             },
-        ]
+        ],
+        stream=True
     )
     
     def stream_response():
-        yield response.choices[0].message.content
-                
-                    
+        for event in response:
+            if event.choices:
+                content = event.choices[0].delta.content
+                if content:
+                    yield content
                     
     return StreamingResponse(stream_response(), media_type='text/plain')
 
